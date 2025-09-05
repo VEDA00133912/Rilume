0859d51e39e465ba356e4c751c74cbc55dd589a2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-09-05 01:25:14 +0000

    userinfoの情報量増やしました。バッジは絵文字の準備ができ次第有効に

---
+++ utils/emoji.js
@@ -0,0 +1,20 @@
+// const badgeEmojis = {
+//   ACTIVE_DEVELOPER: '',
+//   HOUSE_BRAVERY: '',
+//   HOUSE_BRILLIANCE: '',
+//   HOUSE_BALANCE: '',
+//   VERIFIED_DEVELOPER: '',
+//   PARTNERED_SERVER_OWNER: '',
+//   HYPESQUAD_EVENTS: '',
+//   BUG_HUNTER_LEVEL_1: '',
+//   BUG_HUNTER_LEVEL_2: '',
+//   EARLY_SUPPORTER: '',
+//   VERIFIED_BOT: '',
+//   VERIFIED_BOT_DEVELOPER: '',
+// };
+
+// function getBadgeEmojis(flagsArray = []) {
+//   return flagsArray.map(flag => badgeEmojis[flag] || null).filter(Boolean);
+// }
+
+// module.exports = { getBadgeEmojis };
