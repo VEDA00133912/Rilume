2a3efc3546a43e83a4f8499a5522be82674cc7bb
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-09-26 03:11:52 +0000

    アバター、デコレーションリンクボタン追加

--- commands/slashcommands/icon.js
+++ commands/slashcommands/icon.js
@@ -4,6 +4,9 @@ const {
   ApplicationIntegrationType,
   userMention,
   MessageFlags,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
 } = require('discord.js');
 const { createEmbed } = require('../../utils/createEmbed');
 
@@ -28,20 +31,43 @@ module.exports = {
     const user = interaction.options.getUser('user') || interaction.user;
 
     if (!user) {
-      await interaction.reply({
+      return interaction.reply({
         content: 'ユーザーが見つかりませんでした',
         flags: MessageFlags.Ephemeral,
       });
-
-      return;
     }
 
-    const icon = user.displayAvatarURL({ size: 2048, forceStatic: false });
+    const avatarUrl = user.displayAvatarURL({ size: 1024, forceStatic: false });
+
+    const asset = user.avatarDecorationData?.asset;
+    const decorationUrl = asset
+      ? `https://cdn.discordapp.com/avatar-decoration-presets/${asset}.png`
+      : null;
+
     const embed = createEmbed(interaction, {
       description: `**${userMention(user.id)} のアイコン**`,
-      image: icon,
+      image: avatarUrl,
     });
 
-    await interaction.reply({ embeds: [embed] });
+    const buttons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel('アイコン')
+        .setStyle(ButtonStyle.Link)
+        .setURL(avatarUrl),
+    );
+
+    if (decorationUrl) {
+      buttons.addComponents(
+        new ButtonBuilder()
+          .setLabel('アバターデコレーション')
+          .setStyle(ButtonStyle.Link)
+          .setURL(decorationUrl),
+      );
+    }
+
+    await interaction.reply({
+      embeds: [embed],
+      components: [buttons],
+    });
   },
 };
